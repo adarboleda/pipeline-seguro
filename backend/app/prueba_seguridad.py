cf8c6c018cf2fb7a5f47ddeed2f4a8ee15805c41
cmbonifaz <mateobvc@hotmail.com>
2026-06-16 18:00:38 -0500

    prueba 10

--- backend/app/prueba_seguridad.py
+++ backend/app/prueba_seguridad.py
@@ -6,8 +6,5 @@ import hashlib
 import sqlite3
 import requests
 
-def sqli_fstring_operator(db_conn, input_usuario: str):
-    cursor = db_conn.cursor()
-    query = f"SELECT * FROM auditorias WHERE descripcion = {input_usuario}"
-    cursor.execute(query)
-    return cursor.fetchall()
+def deserializacion_insegura_pickle(data_serializada):
+    return pickle.loads(data_serializada)

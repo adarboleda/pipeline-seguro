2d452817aa4bca150556e26d79143f3250f344db
cmbonifaz <mateobvc@hotmail.com>
2026-06-16 17:59:13 -0500

    prueba 9

--- backend/app/prueba_seguridad.py
+++ backend/app/prueba_seguridad.py
@@ -6,8 +6,8 @@ import hashlib
 import sqlite3
 import requests
 
-def sqli_plus_operator(db_conn, input_usuario: str):
+def sqli_fstring_operator(db_conn, input_usuario: str):
     cursor = db_conn.cursor()
-    query = "SELECT * FROM reservas WHERE id_evento = " + input_usuario
+    query = f"SELECT * FROM auditorias WHERE descripcion = {input_usuario}"
     cursor.execute(query)
     return cursor.fetchall()

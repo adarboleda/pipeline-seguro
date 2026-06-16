9acbfbe6f2199dcca4b4f9fa382da915d9d57ab0
cmbonifaz <mateobvc@hotmail.com>
2026-06-16 17:55:45 -0500

    prueba 7

--- backend/app/prueba_seguridad.py
+++ backend/app/prueba_seguridad.py
@@ -6,8 +6,8 @@ import hashlib
 import sqlite3
 import requests
 
-def sqli_format_concatenation(db_conn, input_usuario: str):
+def sqli_percent_operator(db_conn, input_usuario: str):
     cursor = db_conn.cursor()
-    query = "SELECT * FROM usuarios WHERE email = '{}'".format(input_usuario)
+    query = "SELECT * FROM productos WHERE nombre = '%s'" % input_usuario
     cursor.execute(query)
     return cursor.fetchall()

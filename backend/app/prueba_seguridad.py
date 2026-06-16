47fe4fb5dd91bb86acb8d99311aecbb322006a66
cmbonifaz <mateobvc@hotmail.com>
2026-06-16 18:19:38 -0500

    prueba de fuego

--- backend/app/prueba_seguridad.py
+++ backend/app/prueba_seguridad.py
@@ -6,6 +6,8 @@ import hashlib
 import sqlite3
 import requests
 
-def ssrf_untrusted_request(url_usuario: str):
-
-    return requests.get(url_usuario)
+def sqli_format_concatenation(db_conn, input_usuario: str):
+    cursor = db_conn.cursor()
+    query = "SELECT * FROM usuarios WHERE email = '{}'".format(input_usuario)
+    cursor.execute(query)
+    return cursor.fetchall()

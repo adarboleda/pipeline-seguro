9dae1d77a84f8cb65be3982a8871e83f2a8f3f21
cmbonifaz <mateobvc@hotmail.com>
2026-06-16 20:52:57 -0500

    prueba 24

---
+++ backend/app/prueba_seguridad.py
@@ -0,0 +1,14 @@
+import os
+import subprocess
+import pickle
+import yaml
+import hashlib
+import sqlite3
+import requests
+
+
+def path_traversal_open(filename: str):
+
+    ruta = "uploads/" + filename
+    with open(ruta, "r") as archivo:
+        return archivo.read()

2e76c85363de262803279a7de32f996633ab6c05
cmbonifaz <mateobvc@hotmail.com>
2026-06-16 20:19:02 -0500

    prueba 21

--- backend/app/prueba_seguridad.py
+++ backend/app/prueba_seguridad.py
@@ -6,12 +6,12 @@ import hashlib
 import sqlite3
 import requests
 
+
+# Secreto hardcodeado en el código fuente (Fuga de credenciales)
 LLAVE_API_SUPER_SECRETA = "AIzaSyD-1234567890-ABCDE-FGHIJ"
 
-def hash_password_md5(contrasena: str) -> str:
-    """
-    Uso de MD5, un algoritmo criptográfico obsoleto y vulnerable a colisiones.
-    """
-    hasher = hashlib.md5()
+def hash_password_sha1(contrasena: str) -> str:
+
+    hasher = hashlib.sha1()
     hasher.update(contrasena.encode('utf-8'))
     return hasher.hexdigest()
